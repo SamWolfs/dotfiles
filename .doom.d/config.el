d67ba89253c92b0bd22e5fa4baeefd58d597aecf
Sam Wolfs <be.samwolfs@gmail.com>
2022-01-15 15:55:34 +0100

    Remove comments, add ob typescript

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,45 +1,21 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Sam Wolfs"
       user-mail-address "be.samwolfs@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 (setq doom-unicode-font (font-spec :family "MesloLGS NF"))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/dev/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative
       scroll-margin 8)
 
 (setq projectile-project-search-path '(("~/dev/workspaces/" . 2)))
 
 (require 'asdf)
+
+(require 'ob-typescript)
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -82,6 +58,5 @@
                                         (org-present-small)
                                         (org-remove-inline-images)))
 
-(map! :map general-override-mode-map :nv "s" #'evil-substitute)
-
 (remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
